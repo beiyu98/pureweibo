d7aa8df7677503548618e82c49f7d2206ad171d2
bruce <bruceworkzone@outlook.com>
2016-11-27 23:40:08 +0800

    init

--- app/src/main/java/com/brucecode/pureweibo/app/App.java
+++ app/src/main/java/com/brucecode/pureweibo/app/App.java
@@ -7,4 +7,14 @@ import android.app.Application;
  */
 
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
 }
